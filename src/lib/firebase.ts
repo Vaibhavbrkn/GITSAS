ed58550c092cb18e834fc36f101b4464ec3c32f9
vaibhav agrawal <vaibhav.brkn@gmail.com>
2025-01-19 16:07:47 +0530

    firebase changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,13 +9,13 @@ import { error } from "console";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "gitsas-144c9.firebaseapp.com",
-  projectId: "gitsas-144c9",
-  storageBucket: "gitsas-144c9.firebasestorage.app",
-  messagingSenderId: "455203977584",
-  appId: "1:455203977584:web:b32bc03e8581f4356f37b5",
-  measurementId: "G-YWLR5KF4Y9"
+    apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MESUREMENT_ID
 };
 
 // Initialize Firebase
